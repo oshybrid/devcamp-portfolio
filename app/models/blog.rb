3299c8482b644095e6155eca818e4a9b89252fd5
Brad Hutchins <oshybrid@gmail.com>
2018-12-07 17:41:41 -0800

    Implemented validations for blogs, skills and portfolio items

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,7 @@ class Blog < ApplicationRecord
   enum status: {draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged  # changed :name to :title
+
+  # Internal Rails Method
+  validates_presence_of :title, :body
 end
