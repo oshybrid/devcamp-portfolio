d1d37e0ee16ee468622b67e42afc812657305dc2
Brad Hutchins <oshybrid@gmail.com>
2019-01-07 15:17:45 -0800

    Added nav bar, default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   require 'concerns/devise_whitelist_concern.rb'
   require 'concerns/set_source_concern.rb'
   require 'concerns/current_user_concern.rb'
+  require 'concerns/default_page_content_concern.rb'
   include DeviseWhitelistConcern
   include SetSourceConcern
   include CurrentUserConcern
+  include DefaultPageContentConcern
 end
---
+++ app/controllers/concerns/default_page_content_concern.rb
@@ -0,0 +1,12 @@
+module DefaultPageContentConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_page_defaults
+  end
+
+  def set_page_defaults
+    @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @se0_keywords = "Jordan Hudgens Portfolio"
+  end
+end
