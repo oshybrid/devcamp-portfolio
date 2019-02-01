5b4aaa38c7d9cfa548b25f41becbf3ff4828cd99
Brad Hutchins <oshybrid@gmail.com>
2019-02-01 15:39:30 -0800

    Implemented persistent updates for drag and drop interfaces

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -15,6 +15,14 @@ class PortfoliosController < ApplicationController
     @portfolio_individual_item = Portfolio.new
     3.times { @portfolio_individual_item.technologies.build }
   end
+
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+    
+    render nothing: true
+  end
   
   def create
     
