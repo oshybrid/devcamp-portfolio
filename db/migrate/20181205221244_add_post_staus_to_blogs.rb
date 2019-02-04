675a4398b1c71fe4796ce9b3ea708b46893fca06
Brad Hutchins <oshybrid@gmail.com>
2019-02-04 11:14:54 -0800

    Had to fix the migration and DB issues

--- db/migrate/20181205221244_add_post_staus_to_blogs.rb
+++
@@ -1,5 +0,0 @@
-class AddPostStausToBlogs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :blogs, :status, :integer, default: 0
-  end
-end
--- db/migrate/20190130013403_add_position_to_portfolios.rb
+++
@@ -1,5 +0,0 @@
-class AddPositionToPortfolios < ActiveRecord::Migration[5.2]
-  def change
-    add_column :portfolios, :position, :integer
-  end
-end
