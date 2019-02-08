e272f46c0c304882d04ef66bad98795ee72e1711
Brad Hutchins <oshybrid@gmail.com>
2019-02-07 16:08:34 -0800

    Implemented initial file uploading file system via carrierwave

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -24,7 +24,7 @@ class PortfoliosController < ApplicationController
   end
   
   def create
-    
+    @portfolio_individual_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_individual_item.save
@@ -74,6 +74,8 @@ class PortfoliosController < ApplicationController
     params.require(:portfolio).permit(:title, 
                                       :subtitle, 
                                       :body, 
+                                      :main_image,
+                                      :thumb_image,
                                       technologies_attributes: [:name]
                                       )
   end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,6 +7,9 @@ class Portfolio < ApplicationRecord
   
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
+
   def self.angular
     where(subtitle: 'Angular')
   end
@@ -18,7 +21,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400') unless main_image
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200') unless thumb_image
   end
 end
